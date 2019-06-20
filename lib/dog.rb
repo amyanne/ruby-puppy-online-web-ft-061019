6c3639cddbedee2985aa0f4e7741127c75f94771
Amy Westcott <amy.westcott@yahoo.com>
2019-06-20 21:12:40 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,13 @@
 require 'pry'
 class Dog 
+  attr_accessor :name
   @@all = []
  def self.new (name)
    @name = name 
    @@all << @name
    @@all.flatten
  end
- def name
-  @name
- end
+
   def self.clear_all
     @@all.clear 
   end 
